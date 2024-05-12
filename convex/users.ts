2f39aa75dbe0ee089dc7ce1b37e2846255574dd6
Sayantan_627 <27sayantan.pal@gmail.com>
2024-05-12 20:22:33 +0530

    get group members

--- convex/users.ts
+++ convex/users.ts
@@ -67,6 +67,29 @@ export const getMe = query({
   },
 });
 
+export const getGroupMembers = query({
+  args: { conversationId: v.id("conversations") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) {
+      throw new ConvexError("Unauthorized!");
+    }
+    const conversation = await ctx.db
+      .query("conversations")
+      .filter((q) => q.eq(q.field("_id"), args.conversationId))
+      .first();
+
+    if (!conversation) throw new ConvexError("No conversation found");
+
+    const users = await ctx.db.query("users").collect();
+    const groupMembers = users.filter((user) =>
+      conversation.participants.includes(user._id)
+    );
+
+    return groupMembers;
+  },
+});
+
 export const setUserOnline = internalMutation({
   args: { tokenIdentifier: v.string() },
   handler: async (ctx, args) => {
